23773265105dcc50c4f6f20dc2201b2667ddfda8
Vftdan <Vftdan.feed@gmail.com>
2015-09-12 13:01:50 +0300

    Update tool$.js

--- proj/tools/tool$.js
+++ proj/tools/tool$.js
@@ -1,3 +1,13 @@
 var tool$={
+dom:function(a,b){
+if(!b){var b=0};
+a=a.split("");var p=a.shift();var e;c:{
+if(p=="."){e=document.getElementsByClassName(a.join(""))[b];break c};
+if(p=="#"){e=document.getElementById(a.join(""));break c};
+e=document.getElementsByTagName(p+a.join(""))[b]
+};
+addDomFx(e);
+return e
+},
 
 }
