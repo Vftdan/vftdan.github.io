6897254cd23549f132fd21d63ba8542ef9b05107
Vftdan <Vftdan.feed@gmail.com>
2015-12-07 19:19:10 +0300

    Create ui.js

---
+++ host/coc/ui.js
@@ -0,0 +1,15 @@
+window.onscroll=function (){var i;for(i in $UI.scrolled){($UI.scrolled[i])()}};
+var $UI={
+open:function (e,m){
+var t=e.getAttribute("ui");
+t+=" @opened";
+e.setAttribute("ui",t);
+if(m){$UI.scrolled.push(function (){$UI.close(e)})};
+},
+close:function (e){
+var t=e.getAttribute("ui");
+t="@closed";
+e.setAttribute("ui",t);
+},
+scrolled:[]
+}
