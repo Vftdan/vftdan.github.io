03ebc65825c012d6c177b59fa37c605fa24aafca
Vftdan <Vftdan.feed@gmail.com>
2016-08-17 16:13:48 +0400

    Update

--- host/vk/app1/script.js
+++ host/vk/app1/script.js
@@ -16,7 +16,7 @@ function go(e){
 e.preventDefault();
 var p=ge("p").value;
 var h=ge("h").value;
-window.open(ism?(byp?("https://m.vk.com/"+p+"?q=%23"+h):("https://m.vk.com/feed?section=search&q=%23"+h):(byp?("https://vk.com/"+p+"?q=%23"+h):("https://vk.com/feed?c%5Bq%5D=%23"+h+"&section=search")))
+window.open(ism?(byp?("https://m.vk.com/"+p+"?q=%23"+h):("https://m.vk.com/feed?section=search&q=%23"+h)):(byp?("https://vk.com/"+p+"?q=%23"+h):("https://vk.com/feed?c%5Bq%5D=%23"+h+"&section=search")))
 }
 var hex="0123456789ABCDEF".split("");
 function norm(s){
