329e9685ad215a9cb8c4643bd074a0e5371937c0
Vftdan <Vftdan.feed@gmail.com>
2016-08-19 10:48:41 +0400

    Update script.js

--- host/vk/app1/script.js
+++ host/vk/app1/script.js
@@ -10,12 +10,15 @@ if(id=="s")byp=a;
 function ge(id){
 return document.getElementById(id);
 }
+function message(a) {
+if(a!="")alert(a);
+}
 function go(e){
 try{
 e.preventDefault();
 var p=trim(ge("p").value);
 var h=trim(ge("h").value);
-window.open(ism?(byp?("https://m.vk.com/"+p+"?q=%23"+h):("https://m.vk.com/feed?section=search&q=%23"+h+"&_ff=1")):(byp?(function(){throw "В полной версии поиск по странице не поддерживается, установите флажок «Мобильная версия»"}()):("https://vk.com/feed?c%5Bq%5D=%23"+h+"&section=search")))
+window.open(ism?(byp?("https://m.vk.com/"+p+"?q=%23"+h):("https://m.vk.com/feed?section=search&q=%23"+h+"&_ff=1")):(byp?(deskbyp(p,h)):("https://vk.com/feed?c%5Bq%5D=%23"+h+"&section=search")))
 }catch(e){alert(e)}
 }
 function trim(a){
@@ -24,10 +27,13 @@ var i;
 for(i in a){if(a[i].match(/[\n\t ]/))a[i]="";if(a[i]=="/")a[i]="%2F"};
 return a.join("");
 }
-
+function deskbyp(){
+throw "В полной версии поиск по странице не поддерживается, установите флажок «Мобильная версия»"
+}
 
 window.addEventListener('load',(function(){if(window.parent&&window.parent!=window){VK.init(function(){ge("vkb").setAttribute("o",1);
-},function(){alert(-1)},"5.53")}}),false);
+},function(){alert(-1)},"5.53");deskbyp=function(p,h){VK.api("utils.resolveScreenName",{screen_name:p},function(w){window.open("https://vk.com/wall"+w.object_id+"?q=%23"+h)});throw ""}}}),false);
+
 function addtomenu(){
 VK.callMethod("showSettingsBox", 256);
 VK.api("account.setNameInMenu",{},function(){});
