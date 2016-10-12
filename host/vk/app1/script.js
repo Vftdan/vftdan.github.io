faa666f60a2a05853bbfb5a56acd8eef318bec02
Vftdan <Vftdan.feed@gmail.com>
2016-10-12 14:10:33 +0300

    Update script.js

--- host/vk/app1/script.js
+++ host/vk/app1/script.js
@@ -34,7 +34,7 @@ throw "В полной версии поиск по странице не под
 
 window.addEventListener('load',(function(){if(window.parent&&window.parent!=window){VK.init(function(){ge("vkb").setAttribute("o",1);
 },function(){alert(-1)},"5.53");deskbyp=function(p,h){VK.api("utils.resolveScreenName",{screen_name:p},function(w){try{w=w.response;if(w.type=="group"){w.object_id*=-1};var hr="https://vk.com/wall"+w.object_id+"?q=%23"+h;
-var ev1f=function(){document.lastChild.removeEventListener("click",ev1f,false);window.open(hr);message("") };
+var ev1f=function(){document.lastChild.removeEventListener("click",ev1f,false);window.open(hr);try{message("")}catch(e){alert(e)} };
 document.lastChild.addEventListener("click",ev1f,false);
 message("Пожалуйста, кликните ещё раз");
 
