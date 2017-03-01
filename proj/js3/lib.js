c39f47bbde49b426aed526ff05a20b2cad7b731c
Vftdan <Vftdan.feed@gmail.com>
2017-03-01 14:06:33 +0300

    Update lib.js

--- proj/js3/lib.js
+++ proj/js3/lib.js
@@ -143,6 +143,7 @@ return c;
 
 Vectors = {
 Vec: withProto(lib, {copy: function(){return new Vectors.Vec(this.dims)}, toString: function() {return '{' + this.dims.join(', ') + '}'}, resize: function(l) {var i; for(i = this.d; i < l; i++) this.dims[i] = 0; this.d = Math.max(l, this.d)}, addSelf: function(v2){var i; v2 = v2.copy(); Vectors.Vec.toMaxDimNum(this, v2); for(i = 0; i < this.d; i++){this.dims[i] += v2.dims[i]}}, subSelf: function(v2){var i; v2 = v2.copy(); Vectors.Vec.toMaxDimNum(this, v2); for(i = 0; i < this.d; i++){this.dims[i] -= v2.dims[i]}}, mulSelf: function(m){
+if(!m) {var i; for(i=0;i<this.d;i++){this.dims[i] = 0}}
 switch(m.constructor){
 case Number: var i; for(i=0;i<this.d;i++)this.dims[i]*=m; break;
 case Vectors.SqMatrix: var i,j,l = this.d; m.expandTo(l); var a = this.dims.slice(0), b = m.vals; for(i=0;i<l;i++){this.dims[i]=0;for(j=0;j<l;j++){this.dims[i]+=a[j]*b[i][j]}}; break;
@@ -151,6 +152,7 @@ default: break;
 }}, function(a){
 var L;
 //alert(a)
+if(!a) a = [];
 if(a.constructor != [].constructor) {a = [].slice.call(arguments, 0)} else {a = a.slice(0)};
 L = a.length;
 this.d = L;
@@ -161,7 +163,7 @@ sub: function(v1, v2) {v1 = v1.copy(); v1.subSelf(v2); return v1},
 mul: function(v1, v2) {v1 = v1.copy(); v1.mulSelf(v2); return v1}
 }),
 deg: function(a) {return DEG * a},
-SqMatrix: withProto(lib, {toString: function(){return '['+this.vals.join('\n')+']'}, addDim: function(){var i, l = this.length, m = this.vals; m[l] = []; m[l].toString = function(){return this.join('\t')}; for(i=0; i<l; i++){m[i][l] = 0; m[l][i] = 0}; m[l][l] = 1; this.length++ }, expandTo: function(a){var i; for(i = this.length; i < a; i++) this.addDim()}, mulSelf: function(m2){var m1 = this.constructor.mul(m2); this.length = m1.length; this.vals = m1.vals}}, function(m){
+SqMatrix: withProto(lib, {toString: function(){return '['+this.vals.join('\n')+']'}, addDim: function(){var i, l = this.length, m = this.vals; m[l] = []; m[l].toString = function(){return this.join('\t')}; for(i=0; i<l; i++){m[i][l] = 0; m[l][i] = 0}; m[l][l] = 1; this.length++ }, expandTo: function(a){var i; for(i = this.length; i < a; i++) this.addDim()}, mulSelf: function(m2){var m1 = this.constructor.mul(this, m2); this.length = m1.length; this.vals = m1.vals}}, function(m){
 if(arguments.length == 0) m = [];
 if(arguments.length > 1 || (arguments[0] && arguments[0].length === undefined)) m = [].slice.call(arguments, 0);
 var i, j, l = m.length;
@@ -179,7 +181,9 @@ this.vals = m;
 }, {toMaxLength: function(m1,m2){var l=Math.max(m1.length,m2.length);m1.expandTo(l);m2.expandTo(l);return l}, mul: function(a,b){var l=this.toMaxLength(a,b); var c=new Vectors.SqMatrix(); c.expandTo(l); var i,j,r; for(i=0;i<l;i++)for(j=0;j<l;j++){c.vals[i][j]=0;for(r=0;r<l;r++)c.vals[i][j]+=a.vals[i][r]*b.vals[r][j]};return c}
 ,rotMx: function(a, d) {d = d || [0, 1]; if(d.split){d = d.split(''); d = d.map(function(c){switch(c){case 'x':return 0; case 'y':return 1; case 'z':return 2; default:return 3;}})}; var l = Math.max(d[0], d[1])+1; var Mx = new this(); Mx.expandTo(l); var M = Mx.vals; var d1=d[0], d2=d[1]; var cos = Math.cos(a), sin = Math.sin(a); M[d1][d1] = M[d2][d2] = cos; M[d1][d2] = -sin; M[d2][d1] = sin; return Mx}
 }),
-Transformator: withProto(lib, {toString: function(){return 'Transformator:\n' + this.m}, rot: function(a, d){with(Vectors.SqMatrix){this.m = mul(this.m, rotMx(a, d)); return this}}, proceedSelf: function(a){var b = []; for(i=0; i<a.length; i++) {b[i] = a[i]; if(a[i].mulSelf) a[i].mulSelf(this.m)}}}, function(){var m = new Vectors.SqMatrix(); this.m = m})
+Transformator: withProto(lib, {toString: function(){return 'Transformator:{\n' + this.m + '\n' + this.v +'\n}'}, mulMx: function(m){/*alert(this.m.mulSelf);*/ this.m.mulSelf(m); /*alert(this.v.mulSelf);*/ this.v.mulSelf(m); return this}, rot: function(a, d){with(Vectors.SqMatrix){var m = rotMx(a, d); return this.mulMx(m)}}, mov: function(v){}, proceedSelf: function(a){var i; for(i=0; i<a.length; i++) {if(a[i].mulSelf) a[i].mulSelf(this.m); if(a[i].addSelf) a[i].addSelf(this.v)}; return a}, proceed: function(a){var i, b = [], c; for(i=0; i<a.length; i++) {if(!a[i]) continue; c = a[i].constructor; if(c.mul) {b[i] = c.mul(a[i], this.m); if(b[i].addSelf) b[i].addSelf(this.v)}}}}, function(){
+var m = new Vectors.SqMatrix(); this.m = m; var v = new Vectors.Vec(); this.v = v
+})
 }
 
 
@@ -193,4 +197,4 @@ this.compFloat = compFloat;
 
 window[libName] = this;
 })("lib");
-} catch(e) {alert(e)}
+} catch(e) {alert('init:' + e)}
